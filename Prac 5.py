e93768115405b1dbf145adea056f46a76a6df0f0
S. M. Madhavan <151016570+SMMadhavan@users.noreply.github.com>
2025-12-22 23:41:13 +0530

    Overview on string methods

---
+++ Prac 5.py
@@ -0,0 +1,44 @@
+# Day 13 String methods
+'''
+Strings are a sequence of characters. Strings are used to store text data. Strings are immutable, which means that once a string is created, it cannot be changed. Strings are enclosed in single quotes (' ') or double quotes (" "). Strings can be concatenated using the + operator. Strings can be repeated using the * operator. Strings can be indexed using square brackets ([ ]). Strings can be sliced using the colon (:) operator. Strings can be formatted using the format() method. Strings can be converted to uppercase or lowercase using the upper() and lower() methods. Strings can be checked for alphanumeric characters using the isalnum() method. Strings can be checked for alphabetic characters using the isalpha() method. Strings can be checked for numeric characters using the isnumeric() method. Strings can be checked for digits using the isdigit() method. Strings can be checked for whitespace characters using the isspace() method. Strings can be checked for printable characters using the isprintable() method. Strings can be checked for title case using the istitle() method. Strings can be checked for uppercase characters using the isupper() method. Strings can be checked for lowercase characters using the islower() method.
+String methods are built-in functions that can be used to manipulate strings. String methods are used to perform operations on strings.
+String methods are used to perform operations such as:
+ 1. String concatenation 
+ 2. String formatting 3. String splitting 4. String joining 5. String searching 6. String replacing 7. String stripping 8. String counting 9. String finding 10. String checking 11. String converting 12. String formatting
+ 13. String encoding 14. String decoding 15. String comparison 16. String iteration 17. String reversing 18. String sorting 19. String joining 20. String splitting 21. String formatting 22. String searching 23. String replacing 24. String stripping 25. String counting 26. String finding 27. String checking 28. String converting 29. String formatting 30. String encoding 31. String decoding 32. String comparison 33. String iteration 34. String reversing 35. String sorting
+ 36. String joining 37. String splitting 38. String formatting 39. String searching 40. String replacing 41. String stripping 42. String counting 43. String finding 44. String checking 45. String converting 46. String formatting 47. String encoding 48. String decoding 49. String comparison 50. String iteration 51. String reversing 52. String sorting 53. String joining 54. String splitting 55. String formatting 56. String searching 57. String replacing 58. String stripping 59. String counting 60. String finding 61. String checking 62. String converting
+ 63. String formatting 64. String encoding 65. String decoding 66. String comparison 67. String iteration 68. String reversing 69. String sorting 70. String joining 71. String splitting 72. String formatting 73. String searching 74. String replacing 75. String stripping 76. String counting 77. String finding 78. String checking 79. String converting 80. String formatting 81. String encoding 82. String decoding 83. String comparison 84. String iteration 85. String reversing 86. String sorting
+ 87. String joining 88. String splitting 89. String formatting 90. String searching 91. String replacing 92. String stripping 93. String counting 94. String finding 95. String checking 96. String converting 97. String formatting 98. String encoding 99. String decoding 100. String comparison 101. String iteration 102. String reversing 103. String sorting
+ 104. String joining 105. String splitting 106. String formatting 107. String searching 108. String replacing 109. String stripping 110. String counting 111. String finding 112. String checking 113. String converting 114. String formatting 115. String encoding 116. String decoding 117. String comparison 118. String iteration 119. String reversing 120. String sorting and many more.
+ '''
+
+# @ upper()
+# The upper() method is used to convert a string to uppercase.
+# The upper() method returns a new string with all the characters in uppercase.
+# The upper() method does not modify the original string.
+# The upper() method is case-sensitive.
+# The upper() method does not take any arguments.
+
+str="kingslayer"
+print(str.upper()) #Output: KINGSLAYER
+
+# @ lower()
+# The lower() method is used to convert a string to lowercase.
+# The lower() method returns a new string with all the characters in lowercase.
+# The lower() method does not modify the original string.
+# The lower() method is case-sensitive.
+# The lower() method does not take any arguments.
+
+abc="MOOnnIGHT"
+print(abc.lower()) #Output: moonnight
+
+# @ rstrip()
+# The rstrip() method is used to remove trailing (Those which occur after a string) whitespace from a string.
+# The rstrip() method returns a new string with all the trailing whitespace removed.   
+# The rstrip() method does not modify the original string.
+# The rstrip() method takes an optional argument that specifies the characters to be removed.
+# The rstrip() method is case-sensitive.
+# The rstrip() method does not take any arguments.
+
+efg="!!!!!Hello world!!!!!!!"
+print(efg.rstrip("!"))
