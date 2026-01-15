470bf26669e337a593dce9fdc0534c3526515517
S. M. Madhavan <151016570+SMMadhavan@users.noreply.github.com>
2026-01-15 21:12:56 +0530

    Improved Readability

--- Prac 1.py
+++ Prac 1.py
@@ -1,4 +1,5 @@
 #Day 1 (Learning basic print statements for strings and numbers)
+
 print("Hello World, It's my first day in learning python")
 print(1)
 print(3-2)
@@ -7,6 +8,7 @@ print(10/10)
 print(0+1)
 
 #Day 2 (Comments and escape sequence characters)
+
 '''
 This is a multiline character used to make multiple lines as comment lines
 This is line number second
@@ -23,6 +25,7 @@ print("I am learning this course from code with harry\nand this is very interest
 print("Hey",5,6, sep="~",end="009\n")# sep is the command used for seperating different values specified in a single print statement; end command tells the interpretor what should be added to the end of a statement and can be specified only once. If \n is not added to end value, the next line also gets printed on the same line.
 
 #Day 3 (Variables & Data types)
+
 a = 10
 print(a)
 print("The type of a is", type(a))
@@ -33,6 +36,7 @@ c= True
 print("The type of c is", type(c))
 
 #Data types can be classified as Integer, Float, String, Boolean, Complex no.s, Lists, tuple and dictionaries
+
 list=[8,2,"list",2.9]
 print(list)
 tuple=("parrot",34,5.8,"Kings")
