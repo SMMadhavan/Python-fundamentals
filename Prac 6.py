c8c3998881d795f3a4fa80f155d4e66dde47ffec
S. M. Madhavan <151016570+SMMadhavan@users.noreply.github.com>
2026-01-17 23:47:43 +0530

    Add documentation for Python utilities
    Added a docstring explaining Python utilities.

--- Prac 6.py
+++ Prac 6.py
@@ -1,1 +1,7 @@
 Learning python utilities and libraries 
+
+'''
+"Python utilities" generally refers to either the powerful built-in standard library modules for common tasks, 
+the practice of writing reusable "utility functions" in a codebase, or third-party libraries designed for
+general-purpose helpers. 
+'''
