3f8b2e4b847649433f3902ea5bb49daeb1c590b1
S. M. Madhavan <151016570+SMMadhavan@users.noreply.github.com>
2026-02-27 20:02:38 +0530

    Update Prac Ten2.py

--- Prac Ten2.py
+++ Prac Ten2.py
@@ -3,5 +3,8 @@
 '''
 Moving onto Python Visualisation to learn how programming can be visualised
 
+Python data visualization involves using a rich ecosystem of libraries to transform raw data into graphical formats like charts, graphs, 
+and maps to help in analysis and decision-making.
+
 
 '''
