2c702072034b4c31d1596092fb7a078e14a03d2b
S. M. Madhavan <151016570+SMMadhavan@users.noreply.github.com>
2026-02-27 20:01:28 +0530

    Update Prac Ten2.py

--- Prac Ten2.py
+++ Prac Ten2.py
@@ -1,7 +1,7 @@
 # Chronologically python file no.12
 
 '''
-Moving onto Python Visualisation to learn how programming can be visulaised
+Moving onto Python Visualisation to learn how programming can be visualised
 
-This file is desgined to 
+
 '''
