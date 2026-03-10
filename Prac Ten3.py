6d35d394452f84800ed0c76551ab8710312b7e69
S. M. Madhavan <151016570+SMMadhavan@users.noreply.github.com>
2026-03-10 21:54:24 +0530

    Fix formatting issues in Prac Ten3.py

--- Prac Ten3.py
+++ Prac Ten3.py
@@ -5,4 +5,6 @@ Introduction to cps
 
 When we talk about python as a language, we follow the concept of computer programming sense, thinking logic from the computer's (compiler's or interpreter's) point of view.
 Basically, we program logic functionality that the computer performs in its own binary way, not completely in a cognitive sense
+
+Furthermore, it also serves 
 '''
