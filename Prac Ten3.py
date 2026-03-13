9939e1c7e7545211f7215992dad99e452c3b386e
S. M. Madhavan <151016570+SMMadhavan@users.noreply.github.com>
2026-03-13 19:49:33 +0530

    Include everyday life programming examples
    Added examples of programming in everyday devices.

--- Prac Ten3.py
+++ Prac Ten3.py
@@ -6,7 +6,10 @@ Introduction to cps
 When we talk about python as a language, we follow the concept of computer programming sense, thinking logic from the computer's (compiler's or interpreter's) point of view.
 Basically, we program logic functionality that the computer performs in its own binary way, not completely in a cognitive sense
 
-Furthermore, it also serves as the fundamental rule for programming frameworks.
-Now let's have a look on, one of the most interesting and exciting topic .....
+Everyday Life Programming Examples
+Programming is often used in devices around us, often without us realizing it:
 
+Coffee Maker: Setting a timer for 7:00 AM to automatically start brewing is a form of programmed instruction.
+Microwave: Pressing buttons to set time and power level, then executing the cooking sequence.
+Smart Thermostat: Automatically adjusting the home temperature based on user-defined schedules and preferences. 
 '''
