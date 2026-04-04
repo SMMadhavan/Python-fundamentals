e8a0701723562c7a30e2df406c51ed9593d114f7
S. M. Madhavan <151016570+SMMadhavan@users.noreply.github.com>
2026-04-04 23:35:57 +0530

    Enhance description of serialization in implementation
    Clarify the explanation of serialization in pointer analysis.

--- Prac Ten3.py
+++ Prac Ten3.py
@@ -35,5 +35,7 @@ Key aspects of Scratch programming include:
 -Online Community: Users can share their projects, learn from others, and explore projects created by millions of other users worldwide. 
 
 To proceed further with its implementation, lets look into how scratch blends with python and how as a python learner it can be coordinated for 
-understanding serialization (or) sequentialization .........
+understanding serialization (or) sequentialization in pointer analysis of continuous implementation .........
+
+
 '''
