161448f2ea81558fac9063757b215a669ebb547a
S. M. Madhavan <151016570+SMMadhavan@users.noreply.github.com>
2026-04-06 23:39:39 +0530

    Fix typo in comments regarding tradeoffs

--- Prac Ten4.py
+++ Prac Ten4.py
@@ -1,5 +1,5 @@
 # Chronologically python file no.14
 
 '''
-Going further with the concept of tradeoffs between logic and complexity in programmingggggg
+Going further with the concept of ttradeoffs between logic and complexity in programmingggggg
 '''
