8319930db8f4ffd08ffd8a9da4bb2d4ae074c549
S. M. Madhavan <151016570+SMMadhavan@users.noreply.github.com>
2026-04-08 23:40:27 +0530

    Enhance comment on tradeoffs in programming

--- Prac Ten4.py
+++ Prac Ten4.py
@@ -1,5 +1,6 @@
 # Chronologically python file no.14
 
 '''
-Going further with the concept of tradeoffs between logic and complexity in programming
+Going further with the concept of tradeoffs between logic and complexity in programming so that
+space and time complexity can be interpreted in system computation 
 '''
