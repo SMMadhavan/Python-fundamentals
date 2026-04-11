8df36377a2a72bf82dea2f1e711c44565d2f4fce
S. M. Madhavan <151016570+SMMadhavan@users.noreply.github.com>
2026-04-11 23:54:59 +0530

    Add section on steps for tradeoffs in programming
    Added a section outlining steps considered for tradeoffs in programming.

--- Prac Ten4.py
+++ Prac Ten4.py
@@ -4,4 +4,5 @@
 Going further with the concept of tradeoffs between logic and complexity in programming so that
 space and time complexity can be interpreted in system computation and functionality.....
 
+Steps considered for
 '''
