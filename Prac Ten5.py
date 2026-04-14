bd2194e17d1540cd02836530b22a63104293cc96
S. M. Madhavan <151016570+SMMadhavan@users.noreply.github.com>
2026-04-14 13:26:27 +0530

    Update comment to clarify logic overlapping preference

--- Prac Ten5.py
+++ Prac Ten5.py
@@ -1,5 +1,5 @@
 # Chronologically python file no.15
 
 '''
-Beginning with the concept of logic overlapping preference 
+Beginning with the concept of logic overlapping preference in programming 
 '''
