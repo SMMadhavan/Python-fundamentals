54e1948627bdf332f0a3ce44417dc66e20561713
S. M. Madhavan <151016570+SMMadhavan@users.noreply.github.com>
2026-04-15 22:53:09 +0530

    Fix punctuation in comments in Prac Ten5.py

--- Prac Ten5.py
+++ Prac Ten5.py
@@ -1,5 +1,6 @@
 # Chronologically python file no.15
 
 '''
-Beginning with the concept of logic overlapping or augmentation preference in programming 
+Beginning with the concept of logic overlapping or augmentation preference in programming.
+So one thing to get familiar while programming is logical preference of command or instruction execution in a sequence 
 '''
