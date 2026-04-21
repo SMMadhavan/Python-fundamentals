93ebbbb39a61095f67d59b50c7c7f90a93ab7e3a
S. M. Madhavan <151016570+SMMadhavan@users.noreply.github.com>
2026-04-21 22:00:35 +0530

    Fix comment for logical preference explanation
    Corrected a sentence for clarity in the comments.

--- Prac Ten5.py
+++ Prac Ten5.py
@@ -3,7 +3,7 @@
 '''
 Beginning with the concept of logic overlapping or augmentation preference in programming.
 
-So one thing to get familiar while programming is logical preference of command or instruction execution in a sequence that 
+So one thing to get familiar while programming is logical preference of command or instruction execution in a sequence, that 
 influences the output or result of the code.
 
 1.
