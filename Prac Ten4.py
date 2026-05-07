2786fa3d0cf434f6ed7e66feadebb6a5db00de3f
S. M. Madhavan <151016570+SMMadhavan@users.noreply.github.com>
2026-05-07 23:29:39 +0530

    Add fourth point on algebraic proximity

--- Prac Ten4.py
+++ Prac Ten4.py
@@ -32,4 +32,5 @@ be inferred for understanding this:
 Firstly, the binary parameter...
 Secondly, o(n) Complexity
 Third, bit/byte optimization and binary operations 
+Fourth, the algebraic proximity 
 '''
