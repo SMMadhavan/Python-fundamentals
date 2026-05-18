e2364728aa20c42d67a5d2e5c8e5a681210e8309
S. M. Madhavan <151016570+SMMadhavan@users.noreply.github.com>
2026-05-18 21:07:13 +0530

    Add clarification to mathematical calculus definition

--- Prac Ten5.py
+++ Prac Ten5.py
@@ -16,5 +16,6 @@ influences the output or result of the code / program.
 
 Mathematical Calculus can be stated as:
 
-The algebraic computation of instructions consisting of 0's and 1's
+The algebraic computation of instructions consisting of 0's and 1's is primarily known as the 
+mathematical calculus 
 '''
