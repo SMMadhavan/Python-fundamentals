7f7387a397dc181dab3be1519232e0b262956035
S. M. Madhavan <151016570+SMMadhavan@users.noreply.github.com>
2026-07-05 20:58:57 +0530

    Fix punctuation in comments for clarity

--- Prac Ten6.py
+++ Prac Ten6.py
@@ -1,4 +1,7 @@
 # Chronologically python file no. 16
 '''
-Now lets understand the concept of synchronous automation of command in programming 
+Now lets understand the concept of synchronous automation of command in programming:
+
+When we instruct the underlying compiler or interpreter with executable programs that are capable of self implementation upon 
+a call from other subsequent command, we call it as synchronous automation of commands
 '''
