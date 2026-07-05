4380f97f398ddcc9e52f0041845944650d72311d
S. M. Madhavan <151016570+SMMadhavan@users.noreply.github.com>
2026-07-05 20:59:36 +0530

    Improve explanation of synchronous automation
    Clarified the explanation of synchronous automation of commands.

--- Prac Ten6.py
+++ Prac Ten6.py
@@ -3,5 +3,7 @@
 Now lets understand the concept of synchronous automation of command in programming:
 
 When we instruct the underlying compiler or interpreter with executable programs that are capable of self implementation upon 
-a call from other subsequent command, we call it as synchronous automation of commands
+a call from other subsequent command, we call it as synchronous automation of commands.
+
+Basically, commands that run on their own without human interpretation, in layman terms.
 '''
